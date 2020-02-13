7555a6538867ca898d72a663347e4a8bca50d9d1
Michael Dong <michael@Michaels-MacBook-Pro.local>
2020-02-12 20:55:33 -0800

    lvn implementation

--- Pass/Transforms/ValueNumbering/ValueNumbering.cpp
+++ Pass/Transforms/ValueNumbering/ValueNumbering.cpp
@@ -30,21 +30,103 @@ struct ValueNumbering : public FunctionPass {
   static char ID;
   ValueNumbering() : FunctionPass(ID) {}
 
-  bool runOnFunction(Function &F) override {
-    errs() << "ValueNumbering: ";
-    errs() << F.getName() << "\n";
+  bool isInMap(map<pair<Value*, int>, int> valueHash, Value *v){
+    for(auto it = valueHash.begin(); it != valueHash.end(); ++it){
+      if(it->first.first == v) return true;
+    }
+    return false;
+  }
 
+  bool runOnFunction(Function &F) override {
         if (F.getName() != func_name) return false;
 
         map<string, int> hash;
 
         for (auto& basic_block : F)
         {
-            int count = 1;
-            Value *tInst;
+            map<Value*, int> vershionHash;
+            map<pair<Value*, int>, int> valueHash;
+            map<int, pair<Value*, int>> valueHash_reversed;
+            map<string, int> exprHash;
+            int counter = 1;
+
             for (auto& inst : basic_block)
             {
-              
+              if (inst.isBinaryOp()){
+                Value *v1 = inst.getOperand(0);
+                Value *v2 = inst.getOperand(1);
+
+                int vn1;
+                int vn2;
+
+                if(!isInMap(valueHash, v1)){
+                  valueHash.insert(make_pair(make_pair(v1, 0), counter));
+                  valueHash_reversed[counter] = make_pair(v1, 0);
+                  vershionHash[v1] = 0;
+                  vn1 = counter;
+                  counter++;
+                } else {
+                  vn1 = valueHash.at(make_pair(v1, vershionHash[v1]));
+                }
+
+                if(!isInMap(valueHash, v2)){
+                  valueHash.insert(make_pair(make_pair(v2, 0), counter));
+                  valueHash_reversed[counter] = make_pair(v2, 0);
+                  vershionHash[v2] = 0;
+                  vn2 = counter;
+                  counter++;
+                } else {
+                  vn2 = valueHash.at(make_pair(v2, vershionHash[v2]));
+                }
+
+                string op;
+                if(inst.getOpcode() == Instruction::Add) op = "+";
+                if(inst.getOpcode() == Instruction::Sub) op = "-";
+                if(inst.getOpcode() == Instruction::Mul) op = "*";
+                if(inst.getOpcode() == Instruction::SDiv) op = "/";
+
+                if(inst.isCommutative() && vn1 > vn2) {
+                  swap(vn1, vn2);
+                }
+
+                string expr = to_string(vn1) + op + to_string(vn2);
+
+                Value* ptr = dyn_cast<Value>(&inst);
+                if(vershionHash.find(ptr) == vershionHash.end()){
+                  vershionHash.insert(make_pair(ptr, 0));
+                }
+
+                if(exprHash.find(expr) == exprHash.end()) {
+                    exprHash[expr] = counter;
+                    if(!isInMap(valueHash, ptr)){
+                        valueHash.insert(make_pair(make_pair(ptr, 0),counter));
+                        valueHash_reversed[counter] = make_pair(ptr, 0);
+                    }
+                    else{
+                        vershionHash[ptr] += 1;
+                        valueHash.insert(make_pair(make_pair(ptr, vershionHash[ptr]),counter));
+                        valueHash_reversed[counter] = make_pair(ptr, vershionHash[ptr]);
+                    }
+                    errs() << to_string(counter) + "=" + expr << "\n";
+                    ++ counter;
+                } else {
+                    int tmp_counter = exprHash[expr];
+
+                    // errs() << "Redunant Computation: " << *ptr << "\n";
+                    if(!isInMap(valueHash, ptr)){
+                        valueHash.insert(make_pair(make_pair(ptr, 0),tmp_counter));
+                        valueHash_reversed[tmp_counter] = make_pair(ptr, 0);
+                    }
+                    else{
+                        vershionHash[ptr] += 1;
+                        valueHash.insert(make_pair(make_pair(ptr, vershionHash[ptr]),tmp_counter));
+                        valueHash_reversed[tmp_counter] = make_pair(ptr, vershionHash[ptr]);
+                    }
+
+                    errs() << to_string(tmp_counter) + "=" + expr << "\n";
+                }
+
+              }
             }
         }
     return false;
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,7 @@
 cd ./Pass/build && cmake -DCMAKE_BUILD_TYPE=Release ../Transforms/ValueNumbering && make -j4 && cd -
 
+fileName=Test
 cd test 
-clang -Xclang -disable-O0-optnone Test.c -O0 -S -emit-llvm -o Test.ll 
-opt Test.ll -mem2reg -S -o Test.bc
-opt -load ../Pass/build/libLLVMValueNumberingPass.so  -ValueNumbering < Test.bc > /dev/null
+clang -Xclang -disable-O0-optnone $fileName.c -O0 -S -emit-llvm -o $fileName.ll 
+opt $fileName.ll -mem2reg -S -o $fileName.bc
+opt -load ../Pass/build/libLLVMValueNumberingPass.so  -ValueNumbering < $fileName.bc > /dev/null 2> $fileName.out
